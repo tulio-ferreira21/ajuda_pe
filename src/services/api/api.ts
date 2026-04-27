385b7e12c5cf55087581e45bcaf4694586c0dd90
Túlio Diego <diegotulio27@gmail.com>
2026-04-27 14:31:38 -0300

    fix:  1nd try - cookies

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const api = axios.create({
-   // baseURL: '/api'
-   baseURL: import.meta.env.VITE_BACK_URL
+   baseURL: '/api'
+   // baseURL: import.meta.env.VITE_BACK_URL
 })
